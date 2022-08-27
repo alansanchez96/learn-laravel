5773a3385fc0740c7b533d2f044aef1bdd749d38
Alan <alaansannchezz@gmail.com>
2022-08-26 22:27:48 -0300

    Optimizacion del controlador CursoController store y update Se optimizo el codigo de estos dos metodos para insertar registros en la db mediante la vista. Asegurandose su insercion de las tablas incluidas desde el Modelo Curso.

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -25,18 +25,21 @@ class CursoController extends Controller
     public function store(StoreCurso $request)
     {
 
-        $curso = new Curso;
+        /* $curso = new Curso;
 
         $curso->nombre = $request->nombre;
         $curso->descripcion = $request->descripcion;
         $curso->categoria = $request->categoria;
-        $curso->save();
+        $curso->save(); */
+
+        // Nuevo metodo para ahorrar lineas de código
+        $curso = Curso::created($request->all());
 
         // Redireccionamos al usuario a la ruta espeficada (.show necesita una referencia para poder traerla) pero Laravel entiende por defecto que si llamamos a la instancia, debe traerse el id. Por lo tanto ->id es opcional.
-        return redirect()->route('cursos.show', $curso->id);
+        return redirect()->route('cursos.show', $curso);
     }
 
-    // Metodo donde ya instancia dentro del parametro por Laravel
+    // Metodo donde ya instancia dentro del parametro por Laravel. Antes estaba $id
     public function edit(Curso $curso /* $id */)
     {
 
@@ -60,12 +63,8 @@ class CursoController extends Controller
         // return $request->all();
         // return $curso;
 
-        $curso->nombre = $request->nombre;
-        $curso->descripcion = $request->descripcion;
-        $curso->categoria = $request->categoria;
+        $curso = Curso::created($request->all());
 
-        $curso->save();
-
-        return redirect()->route('cursos.show', $curso);    // Laravel entiendo que $curso por defecto tomará el ID
+        return redirect()->route('cursos.show', $curso);    // Laravel entiende que $curso por defecto tomará el ID
     }
 }
--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Curso extends Model
 {
     use HasFactory;
+
+    // $fillable está para solo rellenar los registros con solo estas columnas
+    protected $fillable = ['nombre', 'descripcion', 'categoria'];
+
+    // $guarded está solo para ignorar la tabla mencionada
+    /* protected $guarded = ['tabla_a_ignorar'] */
 }
